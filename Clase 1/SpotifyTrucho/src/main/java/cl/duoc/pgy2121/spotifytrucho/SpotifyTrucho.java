7397fbf66a00eec4a4096efc026c2ae653c9c7e5
Andhre <andhrec24@gmail.com>
2023-08-19 09:56:14 -0400

    Clase 1 Tipo de datos

---
+++ Clase 1/SpotifyTrucho/src/main/java/cl/duoc/pgy2121/spotifytrucho/SpotifyTrucho.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.duoc.pgy2121.spotifytrucho;
+
+/**
+ *
+ * @author Andhre
+ */
+public class SpotifyTrucho {
+    
+    public static void main(String[] args) {
+        
+        char sexo = 'M';
+            String nombre = "Sebastian";
+        boolean isValid = false;
+            byte bytes;
+        int edad = 33;
+            float decimal = 3.14f;
+            double doble = 3.10d;
+            long numeroLargo = 212121212;
+            short numeroChiquito = 34;
+            
+        System.out.println("Nombre: " +nombre);
+        System.out.println("Edad: "+edad);
+        System.out.println("Activo: "+isValid);
+       
+    }
+
+}
